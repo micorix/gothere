341d3bf94316c575af616a1306a7bc825c528b57
micorix <michal.oreziak@gmail.com>
2019-03-26 20:33:45 +0100

    :tada: Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const program = require('commander')
+const fs = require('fs')
+const fse = require('fs-extra')
+const path = require('path')
+const ejs = require('ejs')
+program
+    .version('0.1.0')
+    .option('-c, --config <path>', 'Path to config')
+    .parse(process.argv)
+  
+
+if(!program.config)
+    process.exit()
+
+const configPath = path.join(__dirname, program.config)
+
+const config = require(configPath)
+const contents = fs.readFileSync(path.join('./templates/index.ejs'), 'utf8')
+
+const { name, redirects, fallbackUrl, outFilePath } = config
+const outFile = ejs.render(contents, {
+    name,
+    redirects: JSON.stringify(redirects),
+    fallbackUrl
+})
+fse.emptyDir('./tmp').then(() => {
+    fs.writeFileSync(path.join(__dirname, outFilePath), outFile, 'utf8')
+})
